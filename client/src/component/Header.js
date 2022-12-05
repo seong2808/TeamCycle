87e69432f2f8c80a4d0a9eed5035b5093d16dfa9
seong2808 <seong2808@naver.com>
2022-12-06 06:22:43 +0900

    슬라이드 배너 구현

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -10,9 +10,14 @@ export default function Header(props) {
     return (
         <div className="header">
         <div className="con1">
-            <Link to="/Home">
+            <div className="logo-space">
+            <Link className="link-space" to="/Home">               
                 <img id="logo" src="./logo.png" />
+                <span id="slogan">자전거 이용자를 위한 편리한 웹 서비스</span>
             </Link>
+            </div>
+        
+            
 
             <div className="menu">
             <nav id="main_menu" className="main_menu">
